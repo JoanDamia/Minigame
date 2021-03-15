806f79516378e4d596d0a9dac216d221a54f4c97
PauAl19 <palmendrote@gmail.com>
2021-03-15 18:34:27 +0100

    musica fondo

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -146,9 +146,13 @@ void Start()
 
 	// L4: TODO 1: Init audio system and load music/fx
 	// EXTRA: Handle the case the sound can not be loaded!
-
+	Mix_Init(MIX_INIT_OGG);
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024);
+	state.music = Mix_LoadMUS("Assets/Music.ogg");
+	state.fx_shoot = Mix_LoadWAV("Assets/laser.wav");
 
 	// L4: TODO 2: Start playing loaded music
+	Mix_PlayMusic(state.music, -1);
 
 
 	// Init game variables
@@ -162,7 +166,9 @@ void Start()
 void Finish()
 {
 	// L4: TODO 3: Unload music/fx and deinitialize audio system
-
+	Mix_FreeMusic(state.music);
+	Mix_CloseAudio();
+	Mix_Quit();
 
 	// Unload textures and deinitialize image system
 	SDL_DestroyTexture(state.background);
